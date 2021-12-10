ba576da7d1fcad5e192b913fa3d2abe88d3cc906
LAPTOP-QT5U00E6\ThanhTu <nguyentyvip12@gmail.com>
2021-12-11 00:43:54 +0700

    test

--- Shop8/Controllers/ProductController.cs
+++ Shop8/Controllers/ProductController.cs
@@ -12,9 +12,12 @@ namespace Shop8.Controllers
         // GET: Categories
         public ActionResult Index()
         {
-            //var productDao = new ProductDao();
-            //ViewBag.AllProducts = productDao.ListNewProduct(500);
-
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            var productDao = new ProductDao();
+            ViewBag.AllProducts = productDao.ListNewProduct(24);
             return View();
         }
         [ChildActionOnly]
@@ -30,11 +33,26 @@ namespace Shop8.Controllers
             return PartialView(model);
         }
 
-        public ActionResult Category(long id)
+        public ActionResult Category(long id,int pageIndex = 1,int pageSize = 2)
         {
-            var category = new ProductCategoryDao().ViewDetail(id);
-           // ViewBag.Category = category;
-            return View(category);
+            var category = new CategoryDao().ViewDetail(id);
+            ViewBag.Category = category;
+            int totalRecord = 0;
+            var model = new ProductDao().ListByCategoryId(id, ref totalRecord, pageIndex, pageSize);
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = pageIndex;
+
+            int maxPage = 5;
+            int totalPage = 0;
+            totalPage = (int)Math.Ceiling((double)(totalRecord / pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.maxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = pageIndex + 1;
+            ViewBag.Prev = pageIndex - 1;
+
+            return View(model) ;
         }
 
         public ActionResult Detail(long id)
