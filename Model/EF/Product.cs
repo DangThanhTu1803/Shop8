5b00dbb7f0b9a212afd71e29e04673efc22b87c6
viet <vietng50@gmail.com>
2021-12-15 21:01:08 +0700

    sửa lặt vặt

--- Model/EF/Product.cs
+++ Model/EF/Product.cs
@@ -68,6 +68,13 @@ namespace Model.EF
 
         public DateTime? TopHot { get; set; }
 
+        [StringLength(500)]
+        public string Description1 { get; set; }
+        [StringLength(500)]
+        public string Description2 { get; set; }
+        [StringLength(500)]
+        public string Description3 { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
